19408d6cba676efa0942349566619d0dcd8966de
Aaryatech <code@atsinfosoft.com>
2018-09-17 18:37:46 +0530

    catDistRepo//Sac

--- src/main/java/com/ats/godaapi/repository/reportrepo/CategoryDistReportRepo.java
+++ src/main/java/com/ats/godaapi/repository/reportrepo/CategoryDistReportRepo.java
@@ -13,7 +13,7 @@ public interface CategoryDistReportRepo extends JpaRepository<CategoryDistReport
 	@Query(value = "SELECT m_category.cat_id,m_category.cat_eng_name,m_category.cat_mar_name, COALESCE((select SUM(t_order_detail.order_qty) "
 			+ "from t_order_detail,t_order_header,m_item where t_order_detail.item_id=m_item.item_id AND m_item.cat_id=m_category.cat_id AND "
 			+ "t_order_header.order_header_id=t_order_detail.order_header_id AND t_order_header.order_date BETWEEN :fromDate AND :toDate AND "
-			+ "t_order_header.dist_id=:distId AND t_order_header.order_status=:orderStatus),0) as order_qty,"
+			+ "t_order_header.dist_id IN (:distIdList) AND t_order_header.order_status=:orderStatus),0) as order_qty,"
 			+ ""
 			+ "COALESCE((select SUM(t_order_detail.item_total) from t_order_detail,t_order_header,m_item where "
 			+ "t_order_detail.item_id=m_item.item_id AND m_item.cat_id=m_category.cat_id AND "
