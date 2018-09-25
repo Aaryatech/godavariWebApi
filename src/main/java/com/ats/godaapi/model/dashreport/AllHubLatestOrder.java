47662b735404839f87d7c2c2b70182ba48badfd9
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2018-09-25 16:17:27 +0530

    //atul

--- src/main/java/com/ats/godaapi/model/dashreport/AllHubLatestOrder.java
+++ src/main/java/com/ats/godaapi/model/dashreport/AllHubLatestOrder.java
@@ -3,10 +3,12 @@ package com.ats.godaapi.model.dashreport;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class AllHubLatestOrder {
 	
 	
