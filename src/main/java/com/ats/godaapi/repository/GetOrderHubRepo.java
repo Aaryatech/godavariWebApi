9148b7fafe8d323c3c862ba1c4fe86d35d0ea8aa
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2018-10-03 12:33:12 +0530

    // Neha

--- src/main/java/com/ats/godaapi/repository/GetOrderHubRepo.java
+++ src/main/java/com/ats/godaapi/repository/GetOrderHubRepo.java
@@ -20,7 +20,7 @@ public interface GetOrderHubRepo extends JpaRepository<GetOrderHub, Integer> {
 	@Query(value = "SELECT h.*,d.dist_eng_name,d.dist_mar_name,d.dist_add_mar,d.dist_add_eng ,d.dist_contact_no FROM t_order_header h,m_dist d WHERE  d.dist_id=h.dist_id AND h.order_status=0 AND h.order_date=:currDate  order by h.order_header_id desc", nativeQuery = true)
 	List<GetOrderHub> getOrderHubByStatus(@Param("currDate") String currDate);
 
-	@Query(value = "SELECT h.*, d.dist_eng_name, d.dist_mar_name, d.dist_add_mar, d.dist_add_eng , d.dist_contact_no FROM t_order_header h, m_dist d WHERE d.hub_id=:hubId and  d.dist_id=h.dist_id  AND h.order_type IN (:orderType) AND h.order_status=:orderStatus AND h.order_date=:currDate", nativeQuery = true)
+	@Query(value = "SELECT h.*, d.dist_eng_name, d.dist_mar_name, d.dist_add_mar, d.dist_add_eng , d.dist_contact_no FROM t_order_header h, m_dist d WHERE d.hub_id=:hubId and  d.dist_id=h.dist_id  AND h.order_type IN (:orderType) AND h.order_status=:orderStatus AND h.order_date=:currDate order by h.order_header_id desc", nativeQuery = true)
 	List<GetOrderHub> getOrderHubByHubIdStausAndType(@Param("currDate") String currDate,
 			@Param("orderType") List<Integer> orderType, @Param("orderStatus") int orderStatus,
 			@Param("hubId") int hubId);
